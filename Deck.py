5c1be2da3a2d98875cc5b24d30d6b20bb461b33a
Brian G <brian.gregorius@gmail.com>
2018-12-21 18:37:34 -0500

    added hand files and rans some tests for deck

--- Deck.py
+++ Deck.py
@@ -1,13 +1,18 @@
-from blackjack import suits, ranks, values
+# from blackjack import suits, ranks, values
 import random
-from Card import Card
+from card import Card
+
+suits = ('Heats', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eigth', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
+
 
 class Deck:
     def __init__(self):
         self.deck=[]
         for suit in suits:
             for rank in ranks:
-                self.deck.append(Card(suit,rank))
+                self.deck.append(Card(rank, suit))
     
     def __str__(self):
         deck_comp = ''
@@ -21,3 +26,4 @@ class Deck:
     def deal(self):
         single_card = self.deck.pop()
         return single_card
+
--- blackjack.py
+++ blackjack.py
@@ -1,6 +1,7 @@
 import random
-from Card import Card
-from Deck import Deck
+from card import Card
+from deck import Deck
+from hand import Hand
 
 #variables
 suits = ('Heats', 'Diamonds', 'Spades', 'Clubs')
@@ -8,3 +9,16 @@ ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eigth', 'Nine', 'Ten',
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
 playing = True
 
+
+# test_deck = Deck()
+# print(test_deck)
+
+test_deck = Deck()
+test_deck.shuffle()
+test_player = Hand()
+test_player.add_card(test_deck.deal())
+test_player.add_card(test_deck.deal())
+test_player.value
+
+for card in test_player.cards:
+    print(card)
